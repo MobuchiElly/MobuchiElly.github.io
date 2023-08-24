dc89c2621ae421a68027528a6b82adfa200a23da
MobuchiElly <ugwueleazer@gmail.com>
2023-08-24 15:58:20 +0100

    Stop Watch Project>>JS Generated HTML structure

---
+++ README.md
@@ -0,0 +1,3 @@
+This file was a practice project for me in sightong the functionalities of javascript in the DOM.
+Created a custom HTML element generator function. Function creates element, element attribute, attribute value, and element text simultaneously.
+The main aspect of the project is the STOP WATCH experimenting with functions like setInterval and applying the temp variable method popularly used in sorting algorithms.
---
+++ stopWatchApp.js
@@ -0,0 +1,99 @@
+//Custom function created to createElement and setAttribute simultaneously 
+const CreateElement = (elem, attr, attrValue, _textContent) => {
+    let createdElement = document.createElement(elem);
+    createdElement.setAttribute(attr, attrValue);
+    createdElement.textContent = _textContent;
+    return createdElement;
+}
+
+const body = document.body;
+let h1 = CreateElement('h1', 'class', 'myClass', 'Text Generated with Javascript');
+h1.setAttribute('id', 'main-heading');
+body.appendChild(h1);
+const mainH = document.getElementById("main-heading");
+mainH.style.background="transparent";
+
+
+//STOP WATCH APPLICATION
+//Targeting the html elements
+const startBtn = document.getElementById('start-btn');
+const resetBtn = document.getElementById('reset-btn');
+const stopBtn = document.getElementById('stop-btn');
+const hour = document.querySelector('.hour');
+const min = document.querySelector('.min');
+const sec = document.querySelector('.sec');
+let s =  0, m = 0, h = 0;
+let strS, strM, strH;
+let tempS = 0, tempM = 0, tempH = 0;
+let setTime= 100;
+
+//Function to increment my second(s), minute(m) and hour(h) values
+let runTime = () => {    
+    s++
+    if (s / 60 == 1) {
+        s = 0;
+        m++
+    }
+    if (m / 60 == 1) {
+        m = 0;
+        h++;
+    }
+   
+    tempS = s, tempM = m, tempH = h;
+    strS = s < 10 ? '0' + s: s;
+    strM = m < 10 ? '0' + m : m;
+    strH = h < 10 ? '0' + h : h;
+    sec.textContent = `${strS}`;
+    min.textContent = `${strM}`;
+    hour.textContent = `${strH}`;
+}
+
+let myIntervalId = null;
+startBtn.addEventListener('click', () => {
+    if (!myIntervalId)
+    {
+        myIntervalId = setInterval(runTime, setTime);
+    };
+});
+stopBtn.addEventListener('click', () => {
+    clearInterval(myIntervalId)
+    myIntervalId = null;
+    tempS = s, tempM = m, tempH = h;
+});
+resetBtn.addEventListener('click', () => {
+    clearInterval(myIntervalId)
+    myIntervalId = null;
+    s = 0,  m = 0, h = 0, tempS = 0, tempM = 0, tempH = 0;
+    strS = '00', strM = '00', strH = '00';
+    sec.textContent = `${strS}`;
+    min.textContent = `${strM}`;
+    hour.textContent = `${strH}`;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+/* Read up the following and practice
+e.stopPropagation()
+matches('li')
+e.target.getAttribute
+
+*/
+
+
+
+
+
+
+
+
+
+
